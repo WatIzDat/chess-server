ed5835940b20050091c8a433a7d7ded41bc567ad
WatIzDat <lily3575@hotmail.com>
2025-12-21 23:40:23 -0800

    Implement register/login on frontend

---
+++ ChessServer/ChessServer.ConsoleFrontend/Models/AccessTokenResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ChessServer.ConsoleFrontend.Models;
+
+public class AccessTokenResponse
+{
+    public required string TokenType { get; init; }
+    public required string AccessToken { get; init; }
+    public required string RefreshToken { get; init; }
+    public required int ExpiresIn { get; init; }
+}
--- ChessServer/ChessServer.ConsoleFrontend/Program.cs
+++ ChessServer/ChessServer.ConsoleFrontend/Program.cs
@@ -1,11 +1,68 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+using ChessServer.ConsoleFrontend.Models;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.AspNetCore.SignalR.Client;
+
+JsonSerializerOptions jsonOptions = new()
+{
+    PropertyNameCaseInsensitive = true
+};
+
+using HttpClient client = new();
+
+AccessTokenResponse accessTokenResponse;
+
+while (true)
+{
+    Console.Write("Choose action (register/login): ");
+    string? authAction = Console.ReadLine();
+
+    if (authAction == null || (authAction != "register" && authAction != "login"))
+    {
+        continue;
+    }
+    
+    Console.Write("Email: ");
+    string? email = Console.ReadLine();
+    
+    Console.Write("Password: ");
+    string? password = Console.ReadLine();
+
+    if (email == null || password == null)
+    {
+        continue;
+    }
+
+    HttpContent content = new StringContent(JsonSerializer.Serialize(new { email, password }), mediaType: new MediaTypeHeaderValue("application/json"));
+
+    if (authAction.Equals("register", StringComparison.InvariantCultureIgnoreCase))
+    {
+        await client.PostAsync("http://localhost:5075/register", content);
+    }
+    
+    Console.WriteLine(content);
+    
+    HttpResponseMessage response = await client.PostAsync("http://localhost:5075/login", content);
+    
+    response.EnsureSuccessStatusCode();
+    
+    accessTokenResponse = await response.Content.ReadFromJsonAsync<AccessTokenResponse>(jsonOptions) ?? throw new Exception("Login failed");
+
+    break;
+}
 
 HubConnection gameHubConnection = new HubConnectionBuilder()
-    .WithUrl("http://localhost:5075/gameHub")
+    .WithUrl("http://localhost:5075/gameHub", options =>
+    {
+        options.AccessTokenProvider = GetAccessToken;
+    })
     .WithAutomaticReconnect()
     .Build();
 
-gameHubConnection.On<string>("ReceiveMove", move => Console.WriteLine($"Receieved move: {move}"));
+gameHubConnection.On<string>("ReceiveMove", move => Console.WriteLine($"Received move: {move}"));
 
 await gameHubConnection.StartAsync();
 
@@ -15,4 +72,21 @@ while (true)
     string move = Console.ReadLine()!;
     
     await gameHubConnection.InvokeAsync("SendMove", move);
+}
+
+async Task<string?> GetAccessToken()
+{
+    JsonWebToken jwt = new JsonWebTokenHandler().ReadJsonWebToken(accessTokenResponse.AccessToken);
+
+    if (DateTime.Now <= jwt.ValidTo.Subtract(TimeSpan.FromMinutes(1))) return accessTokenResponse.AccessToken;
+    
+    HttpResponseMessage response = await client.PostAsJsonAsync("http://localhost:5075/refresh", accessTokenResponse.RefreshToken);
+        
+    response.EnsureSuccessStatusCode();
+        
+    accessTokenResponse = await response.Content.ReadFromJsonAsync<AccessTokenResponse>(jsonOptions) ?? throw new Exception("Refresh failed");
+    
+    Console.WriteLine($"AccessToken: {accessTokenResponse.AccessToken}");
+
+    return accessTokenResponse.AccessToken;
 }
