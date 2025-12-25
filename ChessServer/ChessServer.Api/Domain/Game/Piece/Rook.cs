98b06f662a70dc1871900af2f73daa41d96435f0
WatIzDat <lily3575@hotmail.com>
2025-12-25 00:27:25 -0800

    Implement get rook legal squares

--- ChessServer/ChessServer.Api/Domain/Game/Piece/Rook.cs
+++ ChessServer/ChessServer.Api/Domain/Game/Piece/Rook.cs
@@ -4,6 +4,68 @@ public class Rook(PlayerColor color) : Piece(color)
 {
     public override List<Square> GetLegalSquares(Square fromSquare, Board board)
     {
-        throw new NotImplementedException();
+        List<Square> legalSquares = [];
+        
+        AddSlidingPieceAttackSquares(ref legalSquares,
+            fromSquare.File + 1,
+            7,
+            i => new Square(i,
+                fromSquare.Rank),
+            board);
+        
+        AddSlidingPieceAttackSquares(ref legalSquares,
+            fromSquare.File - 1,
+            0,
+            i => new Square(i,
+                fromSquare.Rank),
+            board);
+        
+        AddSlidingPieceAttackSquares(ref legalSquares,
+            fromSquare.Rank + 1,
+            7,
+            i => new Square(fromSquare.File,
+                i),
+            board);
+        
+        AddSlidingPieceAttackSquares(ref legalSquares,
+            fromSquare.Rank - 1,
+            0,
+            i => new Square(fromSquare.File,
+                i),
+            board);
+        
+        return legalSquares;
+    }
+
+    private void AddSlidingPieceAttackSquares(ref List<Square> squares, int from, int to, Func<int, Square> squareProvider, Board board)
+    {
+        if (to == 0)
+        {
+            for (int i = from; i >= to; i--)
+            {
+                if (!CreateAndAddSquare(ref squares, i))
+                    break;
+            }
+        }
+        else
+        {
+            for (int i = from; i <= to; i++)
+            {
+                if (!CreateAndAddSquare(ref squares, i))
+                    break;
+            }
+        }
+
+        return;
+
+        bool CreateAndAddSquare(ref List<Square> squares, int i)
+        {
+            Square square = squareProvider(i);
+
+            if (board.CanOccupySquareWithCaptures(square, Color))
+                squares.Add(square);
+
+            return board.CanOccupySquare(square);
+        }
     }
 }
